3eba907180e416db0b887778a77231cdc1f44c36
Athene Allen <atheneallen93@gmail.com>
2024-06-16 18:18:03 +0100

    add web project template, split dotnet searcher to library for future use

---
+++ GithubDL/GithubSearch.cs
@@ -0,0 +1,18 @@
+﻿namespace GithubDL;
+
+using Octokit;
+public class GithubSearch
+{
+        private GitHubClient client = new GitHubClient(new ProductHeaderValue("IdiotsFirstGithubDownloader"));
+
+        private async Task<SearchRepositoryResult> RunSearch(string searchText)
+        {
+                var request = new SearchRepositoriesRequest(searchText);
+                return await client.Search.SearchRepo(request);
+        }
+
+        private async Task<Release> GetLatestRelease(long repoId)
+        {
+                return await client.Repository.Release.GetLatest(repoId);
+        }
+}
